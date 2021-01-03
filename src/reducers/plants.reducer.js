2f9eb35ac152afd0aac2d95aaacbcb1e1b3fd787
d-otis <dan@dan-foley.com>
2021-01-03 18:37:32 -0500

    define 'SET_PLANTS' plantsReducer case

--- src/reducers/plants.reducer.js
+++ src/reducers/plants.reducer.js
@@ -4,8 +4,29 @@ const INITIAL_STATE = [
 
 const plantsReducer = ( state = INITIAL_STATE, action ) => {
   switch(action.type) {
-    default:
-      return state
+    case "SET_PLANTS":
+      const newState = action.payload.data.map(plant => {
+        return {
+          id: plant.id,
+          name: plant.attributes.name,
+          description: plant.attributes.description,
+          watering: plant.attributes.watering,
+          light: plant.attributes.light,
+          soil: plant.attributes.soil,
+          pestAndDisease: plant.attributes.pest_and_disease,
+          pruning: plant.attributes.pruning,
+          tempLow: plant.attributes.temp_low,
+          tempHi: plant.attributes.temp_hi,
+          fertilization: plant.attributes.fertilization,
+          latinName: plant.attributes.latin_name,
+          roomId: plant.attributes.room.id,
+          seasonIds: plant.attributes.seasons.map(season => season.id)
+        }
+      })
+      return newState
+    
+      default:
+        return state
   }
 }
 
