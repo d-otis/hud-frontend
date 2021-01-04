8888019115fa551311888a62c1c595b8464a47f9
d-otis <dan@dan-foley.com>
2021-01-04 14:26:01 -0500

    add plant.styles styled-component

---
+++ src/components/plants/plant.styles.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const PlantWrapper = styled.div`
+  border: 2px solid purple;
+`;
+
+export const PlantSection = styled.p`
+  border: 3px solid pink;
+  padding-left: 35px;
+`
