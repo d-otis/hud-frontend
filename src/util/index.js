141b21a8653e15e4f474130858ed2f3bd9ac7684
d-otis <dan@dan-foley.com>
2021-01-04 16:54:15 -0500

    add misc/util functions/objects

---
+++ src/util/index.js
@@ -0,0 +1,10 @@
+export const niceNames = {
+  tempLow: "Temperature Low",
+  tempHi: "Temperature High",
+  latinName: "Latin Name",
+  pestAndDisease: "Pest & Disease"
+}
+
+export const capitalize = word => {
+  return word.charAt(0).toUpperCase() + word.substr(1)
+}
