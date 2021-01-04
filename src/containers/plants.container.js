ef0184ccb5cf9a1eadfc6fd1a5e0527a7368a921
d-otis <dan@dan-foley.com>
2021-01-04 12:25:53 -0500

    import and replace h1 with Header styled component

--- src/containers/plants.container.js
+++ src/containers/plants.container.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Header } from '../components/header/header.styles'
 
 
 const PlantsContainer = ({ plants }) => {
@@ -7,7 +8,7 @@ const PlantsContainer = ({ plants }) => {
 
   return(
     <div>
-      <h1>Plants Container!</h1>
+      <Header>Plants Container</Header>
       {plants.map(plant => <li>{plant.name}</li>)}
     </div>
   )
