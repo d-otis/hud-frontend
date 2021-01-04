46256f8b1b048474a8b55016c6934b2eac6fe673
d-otis <dan@dan-foley.com>
2021-01-04 11:35:00 -0500

    implement routing for PlantsContainer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchPlants } from './actions/plants.actions'
-
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import PlantsContainer from './containers/plants.container';
 
 class App extends Component {
 
@@ -13,22 +12,9 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <Route path="/plants" render={renderProps => <PlantsContainer {...renderProps} />} />
+      </Router>
     );
   }
 }
