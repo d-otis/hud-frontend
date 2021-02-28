e7e1cdcd2f2fb6aba5e4c18a27b27e4edc2e9948
d-otis <dan@dan-foley.com>
2021-02-28 16:05:53 -0500

    abstract out current weather section of Weather component

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -1,23 +1,12 @@
 import React from 'react'
+import WeatherCurrent from './WeatherCurrent'
 
 const Weather = ({ weather }) => {
 
   const { current } = weather
 
-  const renderIcons = () => {
-    return current.weather.map(el => <img src={`http://openweathermap.org/img/wn/${el.icon}@2x.png`} alt=""/>)
-  }
-
-  const { temp } = current
-
   return(
-    <>
-    <ul> Le Weather {renderIcons()}
-      <li>Temperature: {temp} &deg; </li>
-      <li>High Temp: xx</li>
-      <li>Low Temp: xx</li>
-    </ul>
-    </>
+    <WeatherCurrent current={current} />
   )
 }
 
---
+++ src/components/weather/WeatherCurrent.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const WeatherCurrent = ({ current }) => {
+
+  const renderIcons = () => {
+    return current.weather.map(el => <img src={`http://openweathermap.org/img/wn/${el.icon}@2x.png`} alt=""/>)
+  }
+
+  const { temp } = current
+
+  return (
+    <ul> Le Weather {renderIcons()}
+      <li>Temperature: {temp} &deg; </li>
+      <li>High Temp: xx</li>
+      <li>Low Temp: xx</li>
+    </ul>
+  )
+}
+
+export default WeatherCurrent;
