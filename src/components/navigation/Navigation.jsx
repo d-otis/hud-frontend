a2960451dae2b2493ef85d334f7fe733cee00285
d-otis <dan@dan-foley.com>
2021-02-28 14:16:56 -0500

    add navbar boiler

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 const Navigation = () => {
 
   return (
-    <h1>Navbar</h1>
+    <>
+    <Navbar bg="light" variant="light">
+      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#features">Features</Nav.Link>
+        <Nav.Link href="#pricing">Pricing</Nav.Link>
+      </Nav>
+    </Navbar>
+  </>
   )
 }
 
