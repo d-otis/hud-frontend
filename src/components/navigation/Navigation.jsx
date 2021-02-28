dd44193a77f4b513660b050c23737cd841c0ace5
d-otis <dan@dan-foley.com>
2021-02-28 14:26:15 -0500

    incorporate Link fixes in NavBar and link up to routes

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import { NavLink } from 'reactstrap'
+import { Link } from 'react-router-dom'
 
 const Navigation = () => {
 
   return (
     <>
-    <Navbar bg="light" variant="light">
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
-      </Nav>
-    </Navbar>
-  </>
+      <Navbar bg="light" expand="md">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <NavLink tag={Link} to="/plants">Plants</NavLink>
+            <NavLink tag={Link} to="/weather">Weather</NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
   )
 }
 
