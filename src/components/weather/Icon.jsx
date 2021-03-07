8753afc28ab5de7388639880dabfc799edaff4d8
d-otis <dan@dan-foley.com>
2021-03-07 12:30:26 -0500

    set up card to render conditionally based on card boolean prop

--- src/components/weather/Icon.jsx
+++ src/components/weather/Icon.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card'
 
-const Icon = ({ iconCode }) => {
+const Icon = ({ weatherData, card }) => {
+  // conditional for card vs image tag
+
+  const { icon, description } = weatherData
+
   return(
-    <p>the icon</p>
+    <>
+      {card 
+        ?
+       <Card.Img variant="top" src={`http://openweathermap.org/img/wn/${icon}@2x.png`} />
+       :
+       <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt={description} key={icon} />
+      }
+    </>
   )
 }
 
