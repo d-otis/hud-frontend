95248b0a074912af9fca2be652f80325e061ad30
d-otis <dan@dan-foley.com>
2021-03-06 19:51:58 -0500

    import and mount Bootstrap components and helpers for Day component

--- src/components/weather/Day.jsx
+++ src/components/weather/Day.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+import { dayMap } from '../../util'
 
 const Day = ({ dayData }) => {
 
   const { dt } = dayData
 
-  const day = dayMap[new Date(dt * 1000).getDay()]
+  const day = dayMap[new Date(dt * 1000).getDay()].slice(0, 3).split("").map(char => char.toUpperCase()).join("")
 
   return(
-    <h2>A Day: { dt }</h2>
+    <Col md className="px-0">
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>{ day }</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in to
+            additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+    </Col>
   )
 }
 
