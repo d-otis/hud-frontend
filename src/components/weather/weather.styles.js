b503ba47289a12c0be89df03e8349a2c7e9669c8
d-otis <dan@dan-foley.com>
2021-03-07 16:00:27 -0500

    center current weather card

--- src/components/weather/weather.styles.js
+++ src/components/weather/weather.styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const WeatherCurrentWrapper = styled.div`
   border: 1px solid pink;
   display: flex;
+  justify-content: center;
 `
 
 export const DayWrapper = styled.div`
