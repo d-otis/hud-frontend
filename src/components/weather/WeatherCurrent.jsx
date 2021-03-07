79817dc01e794c8944bc4cecefd3dc9c0021555e
d-otis <dan@dan-foley.com>
2021-03-07 16:01:51 -0500

    define and use renderShortDescription in WeatherCurrent component

--- src/components/weather/WeatherCurrent.jsx
+++ src/components/weather/WeatherCurrent.jsx
@@ -8,11 +8,15 @@ const WeatherCurrent = ({ current }) => {
     return current.weather.map(data => <Icon weatherData={data} />)
   }
 
+  const renderShortDescription = () => {
+    return current.weather.map(data => data.main).join(" & ")
+  }
+
   const { temp } = current
 
   return (
     <WeatherCurrentWrapper>
-      <ul> Current Weather {renderIcons()}
+      <ul> Current: {renderShortDescription()} {renderIcons()}
         <li>Temperature: {temp} &deg; </li>
         <li>High Temp: xx</li>
         <li>Low Temp: xx</li>
