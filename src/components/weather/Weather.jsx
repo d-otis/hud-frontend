04cc8c0b6912dea4188d5eb657b72d3e12124737
d-otis <dan@dan-foley.com>
2021-03-09 16:55:54 -0500

    import WeatherAlerts component and conditionally mount upon existence of weather alerts in API

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import WeatherCurrent from './WeatherCurrent'
 import SevenDayForecast from './SevenDayForecast'
+import WeatherAlerts from './WeatherAlerts'
 
-const Weather = ({ weather }) => {
+const Weather = ({ weather, alerts }) => {
 
   const { current, daily } = weather
 
   return(
     <>
+      {alerts?.length && <WeatherAlerts alerts={alerts} />}
       <WeatherCurrent current={current} />
       <SevenDayForecast daily={daily} />
     </>
